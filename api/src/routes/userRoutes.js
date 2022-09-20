d48cc3e187ae90370a5d1f6268138c8dcb9f7cf6
cassio <curriculocassio@hotmail.com>
2022-09-20 20:26:32 -0300

    adiciona rotas de evento

--- api/src/routes/userRoutes.js
+++ api/src/routes/userRoutes.js
@@ -1,12 +1,17 @@
 const { Router } = require('express')
 const UserController = require('../controllers/userController')
+const EventController = require('../controllers/eventController')
 const routes = Router()
 
-routes.post('/usuarios', (request, response) =>
-  UserController.createUser(request, response)
+routes.post('/usuarios', (request, response) => UserController.createUser(request, response))
+
+routes.post('/login', (request, response) => UserController.login(request, response))
+
+routes.post('/evento/:userId', (request, response) =>
+  EventController.createEvent(request, response)
 )
-routes.post('/login', (request, response) =>
-  UserController.login(request, response)
+routes.get('/evento', (request, response) =>
+  EventController.getEvent(request, response)
 )
 
 module.exports = routes
