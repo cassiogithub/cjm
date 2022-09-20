ef4edb87d7103a79c478c13b3ab8d61c71ec056f
cassio <curriculocassio@hotmail.com>
2022-09-20 20:24:20 -0300

    add constants

---
+++ api/src/constants/front_url.js
@@ -0,0 +1,3 @@
+const FRONT_URL = 'http://127.0.0.1:5173'
+
+module.exports = FRONT_URL
---
+++ api/src/constants/http-status.js
@@ -0,0 +1,15 @@
+const UNAUTHORIZED = 401
+const BAD_REQUEST = 400
+const NOT_FOUND = 404
+const OK = 200
+const CREATED = 201
+const NO_CONTENT = 204
+
+module.exports = {
+  UNAUTHORIZED,
+  NOT_FOUND,
+  CREATED,
+  NO_CONTENT,
+  BAD_REQUEST,
+  OK,
+}
