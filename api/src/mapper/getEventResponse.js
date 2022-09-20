c698c87af2a539143c782a1bff8ac37b7241ac2d
cassio <curriculocassio@hotmail.com>
2022-09-20 20:26:47 -0300

    add mapper

---
+++ api/src/mapper/getEventResponse.js
@@ -0,0 +1,16 @@
+function getEventResponse(event, user) {
+  return {
+    nome: event.nome,
+    local: event.local,
+    dataEvento: event.data_evento,
+    linkConvite: event.link_convite,
+    hashEvento: event.hash_evento,
+    usuario: {
+      id: user.id,
+      nome: user.nome,
+      urlImagem: user.url_imagem,
+    },
+  }
+}
+
+module.exports = getEventResponse
