e26bd3acd808604bee465fb724a1f41298558de2
Cássio Rodrigues <curriculocassio@hotmail.com>
2022-10-03 14:24:12 -0300

    add verificação

--- app/src/ui/components/home-info-evento/home-info-evento.component.jsx
+++ app/src/ui/components/home-info-evento/home-info-evento.component.jsx
@@ -8,35 +8,39 @@ export function HomeInfoEvento({ event }) {
     addToast('Link copiado com sucesso!')
   }
 
-  return (
-    <ul className="flex flex-col gap-2">
-      <li>
-        <span className="text-sm">Evento</span>
-        <p className="text-lg">{event.nome}</p>
-      </li>
-      <li>
-        <span className="text-sm">Local</span>
-        <p className="text-lg">{event.local}</p>
-      </li>
-      <li>
-        <span className="text-sm">Data/Hora</span>
-        <p className="text-lg">{formatDate(event.data_evento)}</p>
-      </li>
-      <li>
-        <span className="text-sm">Link de cnvite</span>
-        <p className="flex items-center gap-2">
-          <button onClick={copyToClipboard}>
-            <CopyIcon />
-          </button>
-          <a
-            href={event.link_convite}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {event.link_convite}
-          </a>
-        </p>
-      </li>
-    </ul>
-  )
+  function renderEvent() {
+    return (
+      <ul className="flex flex-col gap-2">
+        <li>
+          <span className="text-sm">Evento</span>
+          <p className="text-lg">{event.nome}</p>
+        </li>
+        <li>
+          <span className="text-sm">Local</span>
+          <p className="text-lg">{event.local}</p>
+        </li>
+        <li>
+          <span className="text-sm">Data/Hora</span>
+          <p className="text-lg">{formatDate(event.data_evento)}</p>
+        </li>
+        <li>
+          <span className="text-sm">Link de cnvite</span>
+          <p className="flex items-center gap-2">
+            <button onClick={copyToClipboard}>
+              <CopyIcon />
+            </button>
+            <a
+              href={event.link_convite}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {event.link_convite}
+            </a>
+          </p>
+        </li>
+      </ul>
+    )
+  }
+
+  return <>{event ? renderEvent() : <h2> Nenhum evento encontrado </h2>}</>
 }
