5528535fd9791dbb44657bba9814cbc6692ffefc
Gabriel Henz <gabriel.santos@zallpy.com>
2022-10-05 16:51:04 -0300

    refactor: log server port from env file

--- api/index.js
+++ api/index.js
@@ -1,13 +1,15 @@
 const express = require('express')
-const routes = require('./src/routes/userRoutes')
+const routes = require('./src/routes')
 const cors = require('cors')
 const app = express()
 const dotenv = require('dotenv')
+
 dotenv.config()
+
 app.use(cors())
 app.use(express.json())
 app.use(routes)
 
 app.listen(process.env.PORT, () => {
-  console.log('Server on! :8080')
+  console.log(`Server on! :${process.env.PORT}`)
 })
