f369d777385f384c797998a434d5bba2a802e4a0
Cássio Rodrigues <curriculocassio@hotmail.com>
2022-10-06 02:55:08 -0300

    add configuração tailwind

--- app/tailwind.config.cjs
+++ app/tailwind.config.cjs
@@ -2,7 +2,48 @@
 module.exports = {
   content: ['./src/**/*.jsx'],
   theme: {
-    extend: {},
+    screens: {
+      mobile: { max: '768px' },
+      // => @media (max-width: 768px) { ... }
+
+      laptop: { max: '1024px' },
+      // => @media (max-width: 1024px) { ... }
+
+      desktop: { max: '1280px' },
+      // => @media (max-width: 1280px) { ... }
+
+      sm: '640px',
+      // => @media (min-width: 640px) { ... }
+
+      md: '768px',
+      // => @media (min-width: 768px) { ... }
+
+      lg: '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      xl: '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+    extend: {
+      keyframes: {
+        'fade-in-down': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(-10px)',
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)',
+          },
+        },
+      },
+      animation: {
+        'fade-in-down': 'fade-in-down 0.5s ease-out',
+      },
+    },
   },
   plugins: [],
 }
