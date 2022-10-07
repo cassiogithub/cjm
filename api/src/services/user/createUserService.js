4ee87d0bf66417beac5a7db16ce8fab245e0a014
Cássio Rodrigues <curriculocassio@hotmail.com>
2022-10-07 00:33:31 -0300

    add validação de senha forte

--- api/src/services/user/createUserService.js
+++ api/src/services/user/createUserService.js
@@ -1,4 +1,8 @@
-const { BAD_REQUEST, CREATED, INTERNAL_SERVER_ERROR } = require('../../constants/http-status')
+const {
+  BAD_REQUEST,
+  CREATED,
+  INTERNAL_SERVER_ERROR,
+} = require('../../constants/http-status')
 const bcrypt = require('bcrypt')
 const prismaClient = require('../../database/prismaClient')
 const createUserResponse = require('../../mapper/createUserResponse')
@@ -6,12 +10,44 @@ const createUserResponse = require('../../mapper/createUserResponse')
 async function createUser(request, response) {
   const { nome, email, dataNascimento, urlImagem } = request.body
   const SALT_ROUNDS = 8
+
+  if (request.body.senha.length <= 6) {
+    return response
+      .status(BAD_REQUEST)
+      .send('Sua senha deve conter mais de 6 dígitos')
+  }
+
+  const specialsCharacters = request.body.senha.match(
+    /([~,!,@,#,$,%,^,&,*,-,_,+,=,?,>,<])/
+  )
+
+  if (!specialsCharacters) {
+    return response
+      .status(BAD_REQUEST)
+      .send('Sua senha deve conter caracteres especiais!')
+  }
+
+  const haveNumber = request.body.senha.match(/([0-9])/)
+  if (!haveNumber) {
+    return response
+      .status(BAD_REQUEST)
+      .send('Sua senha deve conter pelomenos 1 valor numérico')
+  }
+
+  if (request.body.senha.trim().length === 0) {
+    return response
+      .status(BAD_REQUEST)
+      .send('A senha não pode ser somente espaços vazios')
+  }
+
   const senha = bcrypt.hashSync(request.body.senha, SALT_ROUNDS)
 
   if (!nome || !email || !dataNascimento || !senha) {
     return response
       .status(BAD_REQUEST)
-      .send('Os campos Nome, Email, data de nascimento e senha, são obrigatórios!')
+      .send(
+        'Os campos Nome, Email, data de nascimento e senha, são obrigatórios!'
+      )
   }
 
   try {
@@ -30,7 +66,9 @@ async function createUser(request, response) {
     return response.status(CREATED).send(userResponse)
   } catch (error) {
     if (error.code === 'P2002') {
-      return response.status(BAD_REQUEST).send('Este email já foi  cadastrado !')
+      return response
+        .status(BAD_REQUEST)
+        .send('Este email já foi  cadastrado !')
     }
 
     return response.status(INTERNAL_SERVER_ERROR).send('erro interno')
